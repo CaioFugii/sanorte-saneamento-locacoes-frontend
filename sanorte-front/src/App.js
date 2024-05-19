5ece7a181bcc36ca7eafa07ccb3905e6cfe25449
NayaraFugii <nayarac.neto@gmail.com>
2024-05-18 22:20:47 -0300

    inicialização do projeto

---
+++ sanorte-front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ButtonFile from './components/button-file/button-file';
+
+function App() {
+    return (
+    <div>
+        <ButtonFile/>
+    </div>
+    );
+}
+
+export default App;
---
+++ sanorte-front/src/components/button-file/button-file.js
@@ -0,0 +1,11 @@
+
+
+function  ButtonFile() {
+    return (
+    <div>
+        <input class="styled" type="button" value="Add to favorites"  src="https://icons8.com.br/icon/12141/file"/>
+    </div>
+    );
+}
+
+export default ButtonFile;
