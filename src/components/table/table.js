0d618baba63ed30d6d2312db86b2b2deed6988c0
Caio Fugii <caio.fugii@hotmail.com>
2024-06-11 18:49:39 -0300

    correcoes tabela

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -3,69 +3,105 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
 import { useState } from 'react';
 import './table.css';
 
-function TableComponent({data}) {
-  return(
+function TableComponent({ data }) {
+  return (
     <Tabs
       defaultActiveKey="home"
       id="uncontrolled-tab-example"
       className="mb-3"
       fill
-      variant='pills'
+      variant="pills"
       transition
     >
-      <Tab eventKey="home" title="Visão Geral">
-      <div className='container'>
-    {data.map((item, index) => {
-      return <div id='tables'><Table striped bordered hover variant="primary" responsive="sm">
-      <thead>
-        <tr>
-          <th key={index}>Referência</th>
-          {Object.keys(item.summary).map((key) => <th>{key}</th>)}
-        </tr>
-      </thead>
-      <tbody>    
-        <tr>
-        <td>
-        {item.tableName}
-        </td>
-        {Object.keys(item.summary).map((key) => <th><InformationModal children={item.values} number={item.summary[key]} classification={key}/></th>)}
-        </tr>
-      </tbody>
-    </Table>
-    </div>
-    })}
-  </div>
+      <Tab eventKey="home" title="Tarefas executadas">
+        <div className="container">
+          {data.map((item, index) => {
+            return (
+              <div id="tables">
+                <Table striped bordered hover variant="primary" responsive="sm">
+                  <thead>
+                    <tr>
+                      <th key={index}>Referência</th>
+                      {Object.keys(item.summary).map((key) => (
+                        <th>{key}</th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{item.tableName}</td>
+                      {Object.keys(item.summary).map((key) => (
+                        <th>
+                          <InformationModal
+                            children={item.values}
+                            number={item.summary[key]}
+                            classification={key}
+                          />
+                        </th>
+                      ))}
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            );
+          })}
+        </div>
       </Tab>
       <Tab eventKey="late" title="Pendentes">
-        Tab content for Profile
+        <div className="container">
+          {data.map((item, index) => {
+            return (
+              <div id="tables">
+                <Table striped bordered hover variant="primary" responsive="sm">
+                  <thead>
+                    <tr>
+                      <th key={index}>Referência</th>
+                      {Object.keys(item.summary).map((key) => (
+                        <th>{key}</th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{item.tableName}</td>
+                      {Object.keys(item.summary).map((key) => (
+                        <th>
+                          <InformationModal
+                            children={item.values}
+                            number={item.summary[key]}
+                            classification={key}
+                          />
+                        </th>
+                      ))}
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            );
+          })}
+        </div>
       </Tab>
-    </Tabs> 
-    
-  )
-
+    </Tabs>
+  );
 }
 
-function InformationModal({children, number, classification}) {
+function InformationModal({ children, number, classification }) {
   const [lgShow, setLgShow] = useState(false);
-  const [dataModal, setDataModal] = useState([]);
-
-  setDataModal([children.find((i) => i.classification === classification)])
-
-
+  const [dataModal, setDataModal] = useState(children);
   return (
     <>
-      <Button variant='link' onClick={() => setLgShow(true)}>{number}</Button>
+      <Button variant="link" onClick={() => setLgShow(true)}>
+        {number}
+      </Button>
       <Modal
-        size="lg"
+        size="xl"
         show={lgShow}
         onHide={() => setLgShow(false)}
         aria-labelledby="example-modal-sizes-title-lg"
+        scrollable
       >
         <Modal.Header closeButton>
           <Modal.Title id="example-modal-sizes-title-lg">
@@ -73,11 +109,41 @@ function InformationModal({children, number, classification}) {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            {dataModal && dataModal.map((item,index)=>{
-       return <p key={index}>   Cidade : {item.origin}</p>
-           
-              
-              })}
+          <div>
+            <Table striped bordered hover variant="primary" responsive="sm">
+              <thead>
+                <tr>
+                  <th>Cidade</th>
+                  <th>Endereco</th>
+                  <th>Tipo</th>
+                  <th>Numero de Servico</th>
+                  <th>Status</th>
+                  <th>Resultado</th>
+                  <th>Classificacao</th>
+                  <th>TSS</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataModal &&
+                  dataModal
+                    .filter((data) => data.classification === classification)
+                    .map((item, i) => {
+                      return (
+                        <tr>
+                          <td key={i}>{item.city}</td>
+                          <td key={i}>{item.address}</td>
+                          <td key={i}>{item.type}</td>
+                          <td key={i}>{item.order_service}</td>
+                          <td key={i}>{item.status}</td>
+                          <td key={i}>{item.result}</td>
+                          <td key={i}>{item.classification}</td>
+                          <td key={i}>{item.tss}</td>
+                        </tr>
+                      );
+                    })}
+              </tbody>
+            </Table>
+          </div>
         </Modal.Body>
       </Modal>
     </>
