24d1e116e19a36cbaf1411891f3e4ac5a1a9f1f3
Caio Fugii <caio.fugii@hotmail.com>
2024-06-16 21:08:26 -0300

    chore

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -3,10 +3,9 @@ import './form.css';
 import { useNavigate } from 'react-router-dom';
 
 function Form() {
-  const USER = 'ADMIN';
   const password = process.env.REACT_APP_PASSWORD;
 
-const [user, setUser] = React.useState('ADMIN');
+  const [user, setUser] = React.useState('ADMIN');
   const [passwordLogin, setPasswordLogin] = React.useState('');
   const [isInvalid, setInvalid] = React.useState(false);
   const navigate = useNavigate();
@@ -15,35 +14,36 @@ const [user, setUser] = React.useState('ADMIN');
     event.preventDefault();
   }
 
-  function handleClick() {
-    if (passwordLogin !== password) {
-      
-      setInvalid(true);     
-    } else {
-      fetch('https://sanorte-backend-cc7f6aa7a173.heroku.com', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body:{
-          user,
-          password,
-        },
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-          return json;
-        });
-      navigate('/file');
-      localStorage.setItem('current_user','ADMIN')
-    }
-  }
+  async function handleClick() {
+    console.log(user);
+    console.log(password);
+    // if (passwordLogin !== password) {
+    //   setInvalid(true);
+    // } else {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/api/login`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            user,
+            password,
+          }),
+        }
+      );
 
-  
+      const teste = await response.json();
+
+      navigate('/file');
+      localStorage.setItem('current_user', 'ADMIN');
+    } catch (error) {
+      setInvalid(true);
+    }
+    // }
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -55,14 +55,14 @@ const [user, setUser] = React.useState('ADMIN');
           alt="SANORTE"
         ></img>
         <div className="container-inputs">
-          <label htmlFor="nome">Usuário: {USER}</label>
-
-          {/* <input
-                    type="text"
-                    id="nome"
-                    value={user}
-                    onChange={(event) => setUser(event.target.value)}
-                /> */}
+          {/* <label htmlFor="nome">Usuário: {USER}</label> */}
+          <input
+            type="text"
+            id="user"
+            value={user}
+            onChange={(event) => setUser(event.target.value)}
+            placeholder="&nbsp;&nbsp;USUÁRIO"
+          />
           <input
             type="password"
             id="nome"
